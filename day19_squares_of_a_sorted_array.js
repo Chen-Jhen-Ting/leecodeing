ea16d14e0eeff6a69971a4ed33d51e91c04b1701
Ted Chen <teddy70642000@gmail.com>
2020-08-30 00:36:14 +0800

    day19 第二種寫法，時間複雜度低的

--- day19_squares_of_a_sorted_array.js
+++ day19_squares_of_a_sorted_array.js
@@ -1,11 +1,32 @@
 input1= [-4,-1,0,3,10]
 output1= [0,1,9,16,100]
 
-Input2= [-7,-3,2,3,11]
+input2= [-7,-3,2,3,11]
 output2= [4,9,9,49,121]
 
 const squaresAndSort = ary => ary.map( x => x**2)
                                  .sort( (x,y)=>{return x-y})
 
+function squaresAndSort2(ary){
+    let start = 0
+    let end = ary.length-1
+    result = []
+    while(start<=end){
+        if( Math.abs(ary[start]) > Math.abs(ary[end]) ){
+            result.unshift(ary[start]**2)
+            start +=1
+        }else{
+            result.unshift(ary[end]**2)
+            end -= 1
+        }
+    }
+    return result
+}
 
-console.log(squaresAndSort(input1))
+const expect = (a,b) =>{ console.log(JSON.stringify(a)==JSON.stringify(b))}
+
+expect(squaresAndSort(input1),output1)
+expect(squaresAndSort(input2),output2)
+console.log("~~~~~~~")
+expect(squaresAndSort2(input1),output1)
+expect(squaresAndSort2(input2),output2)
