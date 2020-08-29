ee295400bd6306cd12e83fd1fe002c8677b821a4
Ted Chen <teddy70642000@gmail.com>
2020-08-29 18:24:31 +0800

    day17 start

--- day17_toeplitz_matrix.js
+++ day17_toeplitz_matrix.js
@@ -4,7 +4,7 @@ matrix1 = [
   [9,5,1,2]
 ]
 output1= True
-Explanation:
+// Explanation: 
 // In the above grid, the diagonals are:
 // "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
 // In each diagonal all elements are the same, so the answer is True.
@@ -15,3 +15,14 @@ matrix2 = [
 output2 = False
 // Explanation:
 // The diagonal "[1, 2]" has different elements.
+
+function diagonal(ary){
+
+}
+
+function expect(a,b){
+  console.log(a==b)
+}
+
+expect(diagonal(matrix1),output1)
+expect(diagonal(matrix2),output2)
