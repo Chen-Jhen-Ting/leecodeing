77ef1704e4d5aecc0823c9abe7460c25e7aeab9b
Ted Chen <teddy70642000@gmail.com>
2020-09-04 20:46:11 +0800

    day21

--- day21_rotate_image.js
+++ day21_rotate_image.js
@@ -1,15 +1,24 @@
-input=[ [1,3,1],
-[1,5,1],
-[4,2,1]]
-
-output =  7
-
-function minimumPath(ary){
-    let x = ary.length-1
-    let y = ary[0].length-1
+input1= [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]]
+output1 = [
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]]
+input2=[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]]
+output2=[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]]
+function rotation(ary){
+    ary
 }
-
 function expect(a,b){
-    console.log(a==b)
+    console.log(JSON.stringify(a)==JSON.stringify(b))
 }
-expect(minimumPath(input),output)
