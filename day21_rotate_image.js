50514b65e3a742a60f1a4a01564678b9159350e3
Ted Chen <teddy70642000@gmail.com>
2020-09-04 22:02:16 +0800

    day21 input 1

--- day21_rotate_image.js
+++ day21_rotate_image.js
@@ -15,10 +15,23 @@ output2=[
   [15,13, 2, 5],
   [14, 3, 4, 1],
   [12, 6, 8, 9],
-  [16, 7,10,11]]
+  [16, 7,10,11]
+]
 function rotation(ary){
-    ary
+  y = ary.length 
+  x = ary[0].length 
+  result = [ [], [], [] ]
+  for(let i = 0 ; i < y ; i++ ){
+    for(let j = 0; j < x ; j++){
+      console.log(ary[j][i])
+      result[j].unshift(ary[i][j])
+    }
+  }
+  // console.log(result)
+  return result
 }
 function expect(a,b){
     console.log(JSON.stringify(a)==JSON.stringify(b))
 }
+expect(rotation(input1),output1)
+// expect(rotation(input2),output2)
