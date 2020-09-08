6a0a4fa742ceb1ad5345e8b7bb84fa7b1410da0b
Ted Chen <teddy70642000@gmail.com>
2020-09-08 14:00:32 +0800

    day 1 finish

---
+++ day_spiral_matrix.js
@@ -0,0 +1,22 @@
+input1=
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+output= [1,2,3,6,9,8,7,4,5]
+input2=
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+output= [1,2,3,4,8,12,11,10,9,5,6,7]
+function spiral(ary,y=0,x=0,result=[]){
+  while(result.length != ary.length*ary[0].length){
+    
+  }
+}
+function expect(a,b){
+  console.log(JSON.stringify(a)==JSON.stringify(b))
+}
