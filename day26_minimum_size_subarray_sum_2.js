5df1d2b88b507054dd4f269ff9f1a9280b2da388
Ted Chen <teddy70642000@gmail.com>
2020-09-13 20:56:11 +0800

    day26 2 finish

--- day26_minimum_size_subarray_sum_2.js
+++ day26_minimum_size_subarray_sum_2.js
@@ -5,7 +5,29 @@ output =  2
 
 function minimumSize( s, ary ){
   let length = 0
-  
+  let start = 0
+  let end = ary.length-1
+  while( start < end ){
+    let sum = ary.slice(start,end+1).reduce((acc,num)=>{ return acc+num },0 )
+    
+    if(sum >= s){
+      length = ary.slice( start , end + 1 ).length
+    }
+
+    if(ary[start] < ary[end]){
+      start += 1
+    }else if (ary[start] > ary[end] ){
+      end -= 1
+    }else{
+      
+      if( ary[start+1] < ary[end-1] ){
+        start +=1
+      }else{
+        end -=1
+      }
+
+    }
+  }
   return length
 }
 
