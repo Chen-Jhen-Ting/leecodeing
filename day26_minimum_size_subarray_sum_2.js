95a75fbf5ab251b3251472594e80bdde31681a77
Ted Chen <teddy70642000@gmail.com>
2020-09-13 20:35:01 +0800

    day26 2 start

---
+++ day26_minimum_size_subarray_sum_2.js
@@ -0,0 +1,16 @@
+// input:
+ s = 7, nums = [2,3,1,2,4,3]
+output =  2
+// Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+
+function minimumSize( s, ary ){
+  let length = 0
+  
+  return length
+}
+
+function expect(a,b){
+  console.log( a == b )
+}
+
+expect( minimumSize( s , nums ) , output )
