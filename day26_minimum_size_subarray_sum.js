680e3fa7f0552d1e2bf4c26f95434f739e65182a
Ted Chen <teddy70642000@gmail.com>
2020-09-13 20:29:28 +0800

    day26 finish 1

--- day26_minimum_size_subarray_sum.js
+++ day26_minimum_size_subarray_sum.js
@@ -4,7 +4,16 @@ output =  2
 // Explanation: the subarray [4,3] has the minimal length under the problem constraint.
 
 function minimumSize( s, ary ){
-
+  let length = 0
+  for( let i = 0 ; i < ary.length; i++){
+    for(let j = i; j< ary.length; j++){
+      let sum = ary.slice(i,j+1).reduce((acc,num)=>{ return acc+num },0)
+      if(sum >= s){
+        length = ary.slice(i,j+1).length
+      }
+    }
+  }
+  return length
 }
 
 function expect(a,b){
